c67cc7684d12244775296144395b5128c08a3617
Zihlu Wang <zihlu.wang@outlook.com>
2023-09-09 01:49:39 +0800

    feat: Initialse project.

---
+++ src/views/IndexView.vue
@@ -0,0 +1,11 @@
+<template>
+  System Index Page
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Welcome to login to our system!</h1><br>
+  Username: <input type="text" v-model="username" /><br />
+  Password: <input type="text" v-model="password" /><br />
+  <button @click="handleLogin">Login</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from "axios"
+
+const username = ref<string>('')
+const password = ref<string>('')
+
+function handleLogin() {
+  axios.post(`http://127.0.0.1:8080/login`, {
+    username: username.value,
+    password: password.value
+  }, {
+    headers: {
+      // You have to use application/x-www-form-urlencoded to login.
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    }
+  }).then((response: unknown) => {
+    console.log(response)
+  })
+}
+</script>
+
+<style scoped></style>
